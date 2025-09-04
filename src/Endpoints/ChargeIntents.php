fc9752bac0c4a7dbe698cc55c55488a56f90af15
Eric Townsend <TOWNSENDEB@YAHOO.COM>
2025-09-04 10:50:36 -0700

    Added Charge Intents Endpoint

---
+++ src/Endpoints/ChargeIntents.php
@@ -0,0 +1,55 @@
+<?php
+declare(strict_types=1);
+namespace Frame\Endpoints;
+
+
+use Frame\Client;
+use Frame\Models\ChargeIntents\ChargeIntent;
+use Frame\Models\ChargeIntents\ChargeIntentCreateRequest;
+use Frame\Models\ChargeIntents\ChargeIntentUpdateRequest;
+
+final class ChargeIntents {
+    private string $basePath = '/v1/charge_intents';
+
+    public function create(ChargeIntentCreateRequest $params): ChargeIntent
+    {
+        $json = Client::create($this->basePath, $params);
+        return ChargeIntent::fromArray($json);
+    }
+
+    public function update(string $id, ChargeIntentUpdateRequest $params): ChargeIntent {
+        $json = Client::update("{$this->basePath}/{$id}", $params);
+        return ChargeIntent::fromArray($json);
+    }
+
+    public function retrieve(string $id): ChargeIntent {
+        $json = Client::get("{$this->basePath}/{$id}");
+        return ChargeIntent::fromArray($json);
+    }
+
+    // TODO: Fix this to be a ChargeIntentListResponse
+    public function list(int $perPage = 10, int $page = 1): array {
+        $json  = Client::get($this->basePath, ['per_page' => $perPage, 'page' => $page]);
+        $data  = isset($json['data']) && is_array($json['data']) ? $json['data'] : [];
+        $items = array_map(
+            fn(array $i) => ChargeIntent::fromArray($i),
+            $data
+        );
+        return $items;
+    }
+
+    public function confirm(string $id, array $params = []): ChargeIntent {
+        $json = Client::create("{$this->basePath}/{$id}/confirm", $params);
+        return ChargeIntent::fromArray($json);
+    }
+
+    public function capture(string $id, array $params = []): ChargeIntent {
+        $json = Client::create("{$this->basePath}/{$id}/capture", $params);
+        return ChargeIntent::fromArray($json);
+    }
+
+    public function cancel(string $id, array $params = []): ChargeIntent {
+        $json = Client::create("{$this->basePath}/{$id}/cancel", $params);
+        return ChargeIntent::fromArray($json);
+    }
+}
