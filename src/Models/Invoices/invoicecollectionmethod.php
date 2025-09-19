aa225475b06ff2932b102e9387d9b0445f54a284
Eric Townsend <TOWNSENDEB@YAHOO.COM>
2025-09-19 09:20:28 -0700

    Added enums for Invoices

---
+++ src/Models/Invoices/invoicecollectionmethod.php
@@ -0,0 +1,8 @@
+<?php
+declare(strict_types=1);
+namespace Frame\Models\Invoices;
+
+enum InvoiceCollectionMethod: string {
+    case AUTO_CHARGE = 'auto_charge';
+    case REQUEST_PAYMENT = 'request_payment';
+}
