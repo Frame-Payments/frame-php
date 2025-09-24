dab4b1006dd53b73e8a3e61989f01b0ae495cf37
Eric Townsend <TOWNSENDEB@YAHOO.COM>
2025-09-24 13:34:09 -0700

    Fixed issues with Endpoint

--- src/Endpoints/InvoiceLineItems.php
+++ src/Endpoints/InvoiceLineItems.php
@@ -9,31 +9,31 @@ use Frame\Models\InvoiceLineItems\LineItemUpdateRequest;
 use Frame\Models\InvoiceLineItems\LineItemListResponse;
 use Frame\Models\Invoices\InvoiceDeletedResponse;
 
-final class Invoices {
-    private const BASE_PATH = '/v1/invoices/';
+final class InvoiceLineItems {
+    private const BASE_PATH = '/v1/invoices';
 
     public function create(string $invoiceId, LineItemCreateRequest $params): InvoiceLineItem {
-        $json = Client::post(self::BASE_PATH . "{$invoiceId}/line_items", $params->toArray());
+        $json = Client::post(self::BASE_PATH . "/{$invoiceId}/line_items", $params->toArray());
         return InvoiceLineItem::fromArray($json);
     }
 
     public function update(string $invoiceId, string $itemId, LineItemUpdateRequest $params): InvoiceLineItem {
-        $json = Client::update(self::BASE_PATH . "{$invoiceId}/line_items/{$itemId}", $params->toArray());
+        $json = Client::update(self::BASE_PATH . "/{$invoiceId}/line_items/{$itemId}", $params->toArray());
         return InvoiceLineItem::fromArray($json);
     }
 
     public function retrieve(string $invoiceId, string $itemId): InvoiceLineItem {
-        $json = Client::get(self::BASE_PATH . "{$invoiceId}/line_items/{$itemId}");
+        $json = Client::get(self::BASE_PATH . "/{$invoiceId}/line_items/{$itemId}");
         return InvoiceLineItem::fromArray($json);
     }
 
     public function list(string $invoiceId, int $perPage = 10, int $page = 1): LineItemListResponse {
-        $json  = Client::get(self::BASE_PATH . "{$invoiceId}/line_items", ['per_page' => $perPage, 'page' => $page]);
+        $json  = Client::get(self::BASE_PATH . "/{$invoiceId}/line_items", ['per_page' => $perPage, 'page' => $page]);
         return LineItemListResponse::fromArray($json);
     }
 
     public function delete(string $invoiceId, string $itemId): InvoiceDeletedResponse {
-        $json = Client::delete(self::BASE_PATH . "{$invoiceId}/line_items/{$itemId}");
+        $json = Client::delete(self::BASE_PATH . "/{$invoiceId}/line_items/{$itemId}");
         return InvoiceDeletedResponse::fromArray($json);
     }
 }
