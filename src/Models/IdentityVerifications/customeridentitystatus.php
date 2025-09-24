a2f61684a5107ebfcc237b9929fe206d082d55cc
Eric Townsend <TOWNSENDEB@YAHOO.COM>
2025-09-24 12:56:26 -0700

    Added enums for Customer Identity

---
+++ src/Models/IdentityVerifications/customeridentitystatus.php
@@ -0,0 +1,10 @@
+<?php
+declare(strict_types=1);
+namespace Frame\Models\IdentityVerifications;
+
+enum CustomerIdentityStatus: string {
+    case INCOMPLETE = 'incomplete';
+    case PENDING = 'pending';
+    case VERIFIED = 'verified';
+    case FAILED = 'failed';
+}
