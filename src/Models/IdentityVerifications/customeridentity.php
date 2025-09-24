9a8278dbba3819917d69310c82ab2de203e1e9e1
Eric Townsend <TOWNSENDEB@YAHOO.COM>
2025-09-24 12:56:13 -0700

    Added models for Customer Identity

---
+++ src/Models/IdentityVerifications/customeridentity.php
@@ -0,0 +1,53 @@
+<?php
+declare(strict_types=1);
+namespace Frame\Models\IdentityVerifications;
+
+final class CustomerIdentity implements \JsonSerializable {
+    public function __construct(
+        public readonly string $id,
+        public readonly CustomerIdentityStatus $status,
+        public readonly ?string $verificationURL,
+        public readonly ?int $pending,
+        public readonly ?int $verified,
+        public readonly ?int $failed,
+        public readonly int $created,
+        public readonly int $updated,
+        public readonly string $object
+    ) {}
+
+    public static function fromArray(array $p): self {
+        $status = null;
+        if (isset($p['status'])) {
+            $status = CustomerIdentityStatus::tryFrom($p['status']);
+            if ($status === null) {
+                error_log("Unexpected CustomerIdentityStatus: " . $p['status']);
+            }
+        }
+
+        return new self(
+            id: $p['id'],
+            status: $status,
+            verificationURL: $p['verification_url'],
+            created: (int)$p['created'],
+            updated: isset($p['updated']) ? (int)$p['updated'] : null,
+            object: $p['object'],
+            pending: (int)$p['pending'],
+            verified: (int)$p['verified'],
+            failed: (int)$p['failed']
+        );
+    }
+
+    public function jsonSerialize(): array {
+        return [
+            'id' => $this->id,
+            'status' => $this->status?->value,
+            'verification_url' => $this->verificationURL,
+            'pending' => $this->pending,
+            'verified' => $this->verified,
+            'failed' => $this->failed,
+            'created' => $this->created,
+            'updated' => $this->updated,
+            'object' => $this->object
+        ];
+    }
+}
